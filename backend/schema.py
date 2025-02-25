59bf1104fc4351dfbd0abf788e67183c00cc1ff2
xllecs <aleexdavidoiu@gmail.com>
2025-02-25 15:15:42 +0200

    updates #2

--- backend/schema.py
+++ backend/schema.py
@@ -24,12 +24,68 @@ class LabelType(DjangoObjectType):
 class Query(graphene.ObjectType):
   tasks = graphene.List(TaskType, status=graphene.String(required=True))
   labels = graphene.List(LabelType)
+  users = graphene.List(UserType)
 
   def resolve_tasks(self, root, status):
     return Task.objects.filter(status=status)
   
   def resolve_labels(self, root):
     return Label.objects.all()
+  
+  def resolve_users(self, root):
+    return User.objects.all()
+  
+class CreateTask(graphene.Mutation):
+  class Arguments:
+    title = graphene.String(required=True)
+    description = graphene.String()
+    status = graphene.String(required=True)
+    priority = graphene.String(required=True)
+
+  task = graphene.Field(lambda: TaskType)
+
+  def mutate(root, info, title, description, status, priority):
+    task = Task(title=title, description=description, status=status, priority=priority)
+    task.save()
+
+    return CreateTask(task=task)
+  
+class UpdateTask(graphene.Mutation):
+  class Arguments:
+    task_id = graphene.ID()
+    title = graphene.String()
+    description = graphene.String()
+
+  task = graphene.Field(lambda: TaskType)
+
+  def mutate(root, info, task_id, title=None, description=None):
+    task = Task.objects.get(id=task_id)
+
+    if title:
+      task.title = title
+
+    if description:
+      task.description = description
+
+    task.save()
+
+    return UpdateTask(task=task)
+
+class SelectAssignee(graphene.Mutation):
+  class Arguments:
+    task_id = graphene.ID(required=True)
+    user_id = graphene.ID(required=True)
+    
+  task = graphene.Field(lambda: TaskType)
+
+  def mutate(root, info, task_id, user_id):
+    task = Task.objects.get(id=task_id)
+    user = User.objects.get(id=user_id)
+
+    task.assignee = user
+    task.save()
+
+    return SelectAssignee(task=task)
 
 class UpdateStatus(graphene.Mutation):
   class Arguments:
@@ -60,6 +116,9 @@ class UpdatePriority(graphene.Mutation):
     return UpdatePriority(task=task)
 
 class Mutation(graphene.ObjectType):
+  create_task = CreateTask.Field()
+  update_task = UpdateTask.Field()
+  select_assignee = SelectAssignee.Field()
   update_status = UpdateStatus.Field()
   update_priority = UpdatePriority.Field()
 
--- backend_app/models.py
+++ backend_app/models.py
@@ -23,7 +23,7 @@ class Task(models.Model):
   description = models.TextField()
   status = models.CharField(max_length=20, choices=TASK_STATUS, default='TO_DO', blank=False)
   reporter = models.ForeignKey(User, on_delete=models.CASCADE, null=False, default=get_default_user(), related_name='reporter_tasks')
-  assignee = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='assignee_tasks')
+  assignee = models.ForeignKey(User, on_delete=models.CASCADE, null=True, blank=True, related_name='assignee_tasks')
   priority = models.CharField(max_length=20, choices=TASK_PRIORITY, default='LOW', blank=False)
   created_at = models.DateField(auto_now_add=True)
   updated_at = models.DateField(auto_now=True)
